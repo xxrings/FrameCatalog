18086ccdb01393b431e8ad945710cc83b2daf880
xxrings <xxrings@gmail.com>
2025-06-16 08:56:39 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-p// ===========================================================================
+// ===========================================================================
 // main.js
 // ===========================================================================
 //
@@ -26,7 +26,6 @@ const CatalogApp = (function() {
         buildFilterControls();
         renderThumbnails(allFrames);
         hookMultiDropdowns();
-      //  setupFilters();
       })
       .catch(err => {
         thumbsContainer.innerHTML = '<p class="error">Failed to load catalog.</p>';
